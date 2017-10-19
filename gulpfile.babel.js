6a450a7c8efe54d727a61fbe723233a6556a3ab7
fedor.k <fedor.kudinov.dvlopbiz@gmail.com>
2017-10-19 17:55:11 +0300

    add space classes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,7 +13,7 @@ gulp.task('sass', () => {
     .pipe(plumber())
     //.pipe(sourcemaps.init())
     .pipe(sass({
-      outputStyle: 'expanded', // nested, expanded, compact, compressed
+      outputStyle: 'nested', // nested, expanded, compact, compressed
       precision: 5,
       sourceComments: false
     }).on('error', sass.logError))
