b83fbf559b219c79c28180364357f74116f3a769
fedor.k <fedor.kudinov.dvlopbiz@gmail.com>
2017-11-14 15:53:38 +0200

    fix theme_support

--- functions.php
+++ functions.php
@@ -18,19 +18,6 @@ require_once locate_template('/functions/widgets.php');
 require_once locate_template('/functions/index-pagination.php');
 require_once locate_template('/functions/split-post-pagination.php');
 require_once locate_template('/functions/feedback.php');
-
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( "title-tag" );
-add_theme_support( "custom-header" );
-add_theme_support( "custom-background" );
-
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-add_theme_support( 'woocommerce' );
-    
-//Translate Theme
-load_theme_textdomain( 'brainworks', get_template_directory().'/languages' ); 
-    
     
 function brainworks_breadcrumbs_localization($l10n) {
   return array(
@@ -48,5 +35,3 @@ function brainworks_breadcrumbs_localization($l10n) {
 }
 
 add_filter('kama_breadcrumbs_default_loc', 'brainworks_breadcrumbs_localization', 10, 1);
-    
-}
--- functions/setup.php
+++ functions/setup.php
@@ -1,15 +1,26 @@
 <?php
 
 function brainworks_setup() {
-	add_editor_style('theme/css/editor-style.css');
+  //Translate Theme
+  load_theme_textdomain( 'brainworks', get_template_directory().'/languages' );
+
 	add_theme_support('post-thumbnails');
 	add_theme_support('custom-logo');
   add_theme_support('widgets');
+  add_theme_support( 'automatic-feed-links' );
+  add_theme_support( "title-tag" );
+  add_theme_support( "custom-header" );
+  add_theme_support( "custom-background" );
+  add_theme_support( 'woocommerce' );
+
+  add_editor_style('theme/css/editor-style.css');
+
 	update_option('thumbnail_size_w', 170);
 	update_option('medium_size_w', 470);
 	update_option('large_size_w', 970);
 }
-add_action('init', 'brainworks_setup');
+
+add_action('after_setup_theme', 'brainworks_setup');
 
 if (! isset($content_width))
 	$content_width = 600;
