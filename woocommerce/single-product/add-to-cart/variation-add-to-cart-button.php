bcd4a09356889527f968b6403aa037bbd19477fc
fedor.k <fedor.kudinov.dvlopbiz@gmail.com>
2018-03-28 16:18:13 +0300

    small fixes;

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -2,38 +2,38 @@
 /**
  * Single variation cart button
  *
- * @see 	https://docs.woocommerce.com/document/template-structure/
+ * @see    https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 3.0.0
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 global $product;
 ?>
 <div class="woocommerce-variation-add-to-cart variations_button">
-	<?php
-		/**
-		 * @since 3.0.0.
-		 */
-		do_action( 'woocommerce_before_add_to_cart_quantity' );
+    <?php
+    /**
+     * @since 3.0.0.
+     */
+    do_action('woocommerce_before_add_to_cart_quantity');
 
-		woocommerce_quantity_input( array(
-			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
-		) );
+    woocommerce_quantity_input(array(
+        'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+        'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+        'input_value' => isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : $product->get_min_purchase_quantity(),
+    ));
 
-		/**
-		 * @since 3.0.0.
-		 */
-		do_action( 'woocommerce_after_add_to_cart_quantity' );
-	?>
-	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-  <button type="button" class="single_buy_one_click_button button alt one-click"><?php _e('Купить в 1 клик','brainworks'); ?></button>
-	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
-	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
-	<input type="hidden" name="variation_id" class="variation_id" value="0" />
+    /**
+     * @since 3.0.0.
+     */
+    do_action('woocommerce_after_add_to_cart_quantity');
+    ?>
+    <button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+    <button type="button" class="single_buy_one_click_button button alt one-click"><?php _e('Buy in one click', 'brainworks'); ?></button>
+    <input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>"/>
+    <input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>"/>
+    <input type="hidden" name="variation_id" class="variation_id" value="0"/>
 </div>
