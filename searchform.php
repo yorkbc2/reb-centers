2817fe9b01386bd09bef3c8e4630567dc7c968b1
Alex Yorke <yorkbc2@gmail.com>
2018-06-12 17:23:23 +0300

    Some fixes

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form class="form-inline navbar-form" role="search" method="get"  id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
     <input class="form-control" type="search" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php _e('Search', 'brainworks') ?>">
     <button class="btn-search" type="submit" id="searchsubmit">
-        <i class="fa fa-search reversed-color" aria-hidden="true"></i>
+        <i class="fa fa-search" aria-hidden="true"></i>
     </button>
 </form>
