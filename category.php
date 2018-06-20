8a5188bc21209dedbd3c906b6cb3c931837c874d
Kudinov Fedor <brothersrabbits@mail.ru>
2018-06-20 17:53:58 +0300

    Change category.php

--- category.php
+++ category.php
@@ -1,16 +1,12 @@
 <?php get_header(); ?>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9">
-            <h1><?php _e('Category', 'brainworks'); ?>: <?php echo single_cat_title(); ?></h1>
-            <hr>
-            <?php get_template_part('loops/content-2', get_post_format()); ?>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-            <?php get_sidebar(); ?>
-        </div>
-    </div><!-- /.row -->
-</div><!-- /.container -->
+<div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+<h1><?php _e('Category', 'brainworks'); ?>: <?php echo single_cat_title(); ?></h1>
+
+<div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+<hr>
+<div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
+
+<?php get_template_part('loops/content-2', get_post_format()); ?>
 
 <?php get_footer(); ?>
