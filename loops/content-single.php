817df283aa0d41f50aa1390f09b342cfec2e7a52
Fedor Kudinov <fedor_kudinov@adoriasoft.com>
2018-06-25 21:55:49 +0300

    Fix the_date;

--- loops/content-single.php
+++ loops/content-single.php
@@ -12,7 +12,7 @@
             <?php /*
             <h5>
                 <span class="text-muted author"><?php _e('By', 'brainworks'); echo " "; the_author() ?></span>
-                <time  class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php echo get_option( 'date_format' ) ?></time>
+                <time class="text-muted" datetime="<?php the_time('d-m-Y')?>"><?php the_date(get_option('date_format')); ?></time>
             </h5>
             <p class="text-muted" style="margin-bottom: 30px;">
                 <i class="fa fa-folder-open-o"></i>&nbsp; <?php _e('Filed under', 'brainworks'); ?>: <?php the_category(', ') ?><br/>
