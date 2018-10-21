f210d5537b817606a8399b58558ac2aca08ce965
YegorMamay <web@brainworks.com.ua>
2018-10-21 12:55:06 +0300

    Some makeup changes by Yegor Mamay

--- page-auth-only.php
+++ page-auth-only.php
@@ -4,12 +4,13 @@
      */
 get_header();
 the_post(); ?>
-    <div class="sp-md-2"></div>
+   
+    <div class="sp-xs-2"></div>
     <?php if(bw_user_logged_in()): ?>
         <?php the_content(); ?>
     <?php else: ?>
-        <h1>You must be logged in to see this page!</h1>
+        <h1><?php _e('You must be logged in to see this page!', 'brainworks') ?></h1>
         <?php echo do_shortcode('[bw-custom-auth]'); ?>
     <?php endif; ?>
-    <div class="sp-md-2"></div>
+    <div class="sp-xs-2"></div>
 <?php get_footer(); ?>
